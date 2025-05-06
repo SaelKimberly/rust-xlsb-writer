d70ade7a7137bfe2684237d11bb906f5c66415e8
Peter <sael.kimberly@yandex.ru>
2025-05-06 18:18:11 +0500

    feat: Add `write_as_empty_biff` and `write_as_biff` methods in `crate::biff`. Very useful for unchecked BIFF record constructing from raw bytes and instant write them to writer, w/o copying data at all.

--- rust-xlsb-core/src/biff.rs
+++ rust-xlsb-core/src/biff.rs
@@ -1,8 +1,11 @@
-use std::io::Write;
+use std::{
+    io::{self, Write},
+    ops::Deref,
+};
 
 use deku::{DekuContainerRead, DekuContainerWrite};
 
-use crate::{CheckedBiff, Error, Result};
+use crate::{BiffHead, CheckedBiff, Error, KnownID, Result};
 
 pub trait BiffWrite: CheckedBiff {
     fn biff_push_to(&self, out: &mut Vec<u8>) -> Result<usize>;
@@ -26,4 +29,21 @@ impl<T: for<'a> DekuContainerRead<'a> + DekuContainerWrite + CheckedBiff> BiffWr
             Err(Error::ValidationError("Invalid BIFF record".to_owned()))
         }
     }
+}
+
+pub fn write_as_empty_biff(id: KnownID, writer: &mut dyn Write) -> io::Result<usize> {
+    let biff_head = BiffHead::new(id as u16, 0).expect("Should never fail");
+    let (raw, len) = biff_head.as_raw_data();
+    writer.write(&raw[..len])
+}
+
+pub fn write_as_biff<W: Write, S: Deref<Target = [u8]>>(
+    id: KnownID,
+    data: S,
+    writer: &mut W,
+) -> io::Result<usize> {
+    let data = data.deref();
+    let biff_head = BiffHead::new(id as u16, data.len() as u32).expect("Should never fail");
+    let (raw, len) = biff_head.as_raw_data();
+    Ok(writer.write(&raw[..len])? + writer.write(data)?)
 }
